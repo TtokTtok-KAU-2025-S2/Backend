ef90730cae1a09fc85ebbb1675105976aa9bcad0
kangscode <2009ksj@kau.kr>
2025-10-02 10:14:38 +0900

    first commit

---
+++ src/test/java/TtokTtok/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package TtokTtok.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
