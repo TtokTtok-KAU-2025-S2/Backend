493b25614b836d6cf49651738f67031c23e03139
DlSeokJin <leesj000128@naver.com>
2025-11-09 16:16:45 +0900

    회원가입 시 이메일 인증 절차 구현

--- src/main/java/TtokTtok/Backend/web/controller/UserController.java
+++ src/main/java/TtokTtok/Backend/web/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/users")
@@ -25,11 +27,14 @@ public class UserController {
          return ApiResponse.onSuccess(UserResponse.UserDetailDto.builder()
                  .id(user.getId())
                  .email(user.getEmail())
-                 .dong(user.getDong())
-                 .hosu(user.getHosu())
-                 .role(user.getRole())
                  .build());
      }
+
+    @GetMapping("/verify-email")
+    public ApiResponse<String> verifyEmail(@RequestParam("email") String email, @RequestParam("code") String code) {
+        userService.verifyEmail(email, code);
+        return ApiResponse.onSuccess("이메일 인증이 성공적으로 완료되었습니다.");
+    }
 
      @PostMapping("/login")
      public ApiResponse<UserResponse.TokenInfo> login(@RequestBody @Valid UserRequest.LoginDto request) {
